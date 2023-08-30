1f77e090bfc961235c0da8bee1c8043d3ab5fba1
The Specialist_JS <103410982+finaleshot2016@users.noreply.github.com>
2023-08-30 22:47:52 +0800

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -14,8 +14,8 @@ export const Footer = () => {
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-            <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer"><img src={navIcon1} alt="" /></a>
-                <a href="
+            <a href="https://twitter.com/MagicForgETH" target="_blank" rel="noopener noreferrer"><img src={navIcon1} alt="" /></a>
+                <a href="https://t.me/magicforg_eth
 "><img src={navIcon2} alt="" /></a>
 
             </div>
